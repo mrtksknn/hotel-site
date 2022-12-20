81c2103a6c7716cfe2df1ac6be69e677547417bb
Mert keskin <mertkeskinmtk6@gmail.com>
2022-12-20 23:25:06 +0300

    add services section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./components/navbar";
+import Services from "./components/services";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <main>
+        <Services />
+      </main>
     </div>
   );
 }
---
+++ src/components/services.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <section id='services' className='icons bg-light'>
+      <div className='flex-items'>
+        <div>
+          <i className='fas fa-concierge-bell fa-2x'></i>
+          <div>
+            <h3>24/7 Front Desk</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur
+              adipising elit. Eius, dolorem?
+            </p>
+          </div>
+        </div>
+        <div>
+          <i className='fas fa-utensils fa-2x'></i>
+          <div>
+            <h3>Restaurant Bar</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur
+              adipising elit. Eius, dolorem?
+            </p>
+          </div>
+        </div>
+        <div>
+          <i className='fas fa-bus fa-2x'></i>
+          <div>
+            <h3>Transfer Services</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur
+              adipising elit. Eius, dolorem?
+            </p>
+          </div>
+        </div>
+        <div>
+          <i className='fas fa-spa fa-2x'></i>
+          <div>
+            <h3>Spa Suites</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur
+              adipising elit. Eius, dolorem?
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Services
