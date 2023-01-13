49644d479f39fe4cb1fbc17a833875e77604e521
Mert keskin <mertkeskinmtk6@gmail.com>
2023-01-13 23:05:47 +0300

    change folder camelcase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Gallery from "./components/Gallery";
-import Navbar from "./components/Navbar";
-import Rooms from "./components/Rooms";
-import Services from "./components/Services";
-import Teams from "./components/Teams";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Gallery from "./Components/Gallery";
+import Navbar from "./Components/Navbar";
+import Rooms from "./Components/Rooms";
+import Services from "./Components/Services";
+import Teams from "./Components/Teams";
 
 function App() {
   return (
